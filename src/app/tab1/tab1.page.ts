f05dc57fb99fe781cb1d8820b14ec128df81b820
Hien Curve <hien@curvetomorrow.com.au>
2023-10-10 17:34:54 +1100

    [app-review] installed and tested - no working - WIP

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppReview } from '@awesome-cordova-plugins/app-review/ngx';
+import { ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements ViewDidEnter{
 
-  constructor() {}
+  appReview = inject(AppReview);
+
+  constructor() {
+
+  }
+
+  ionViewDidEnter(): void {
+    // this.appReview.requestReview().catch(() => {
+    //   return this.appReview.openStoreScreen();
+    // });
+  }
 
 }
